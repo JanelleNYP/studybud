7b415fae924abc7bf7cd7952d92bc6b5a35c06f8
JanelleNY <jnypoku@st.ug.edu.gh>
2025-12-01 23:15:45 +0000

    Migrations, database and admin panel

--- base/models.py
+++ base/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Rooms(models.Model):
+class Room(models.Model):
     #host =
     #topic =
     name = models.CharField(max_length=200)
@@ -10,3 +10,6 @@ class Rooms(models.Model):
     #participants = 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
