721f4dd86e7e1a94a6734b95b4d9bad4b3e4fe24
JanelleNYP <jannypoks@gmail.com>
2025-12-03 23:40:28 +0000

    Admin panel

--- base/models.py
+++ base/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
@@ -24,7 +23,7 @@ class Room(models.Model):
 
 
 class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
