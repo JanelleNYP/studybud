5edc067d61a5bbc07e798cb159183f1d197d173b
JanelleNYP <jannypoks@gmail.com>
2025-12-04 23:57:57 +0000

    CRUD

--- base/views.py
+++ base/views.py
@@ -14,8 +14,13 @@ def home(request):
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
+
 def room(request, pk):
     room = Room.objects.get(id=pk)
         
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
